688ff660e921311b7c805bd0fb70d953ef343c3c
edwin-salinas1 <edwin.salinas@stochasticstudio.com>
2015-10-24 02:34:53 -0700

    sends gcm

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,11 @@
 package controllers
 
+import play.api.libs.json.Json
+import play.api.libs.ws.WS
 import play.api.mvc._
+import play.api.Play.current
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 object Application extends Controller {
 
@@ -17,4 +22,21 @@ object Application extends Controller {
     println(body)
     Ok("Fue POST -> \n" + body)
   }}
+
+  def sendGCM = Action{ implicit req => {
+    val msg = Json.obj("data" -> Json.obj(
+      "score" -> "5x1",
+      "time" -> "15:10"),
+    "to" -> "cQlhpkpFEq4:APA91bGt0eocVRSepSP0TukasdVqsXokrRHi3x65-OBNehy_sSdmztbtBj6Hit4Y21094XeLHGCVY36beHjnKgxjrCU835Tz3YYpBEWqbly4RPENfw0m4wR0BxlNgv1Ghu-DjNHBVDSD")
+
+    val send = WS.url("https://gcm-http.googleapis.com/gcm/send")
+      .withHeaders(
+        ("Content-Type", "application/json"),
+        ("Authorization", "key=AIzaSyDF1bcfh7XGvyeD7RKVsMNW5nG523NR__M"))
+    .post(msg)
+    val res = Await.result(send, Duration.Inf)
+    Ok(res.body)
+  }
+
+  }
 }
