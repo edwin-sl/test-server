b97082f62b0e3cf614f60d2c23645562f2744f73
edwin-salinas1 <edwin.salinas@stochasticstudio.com>
2015-10-24 17:12:31 -0700

    send notification and messages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,8 @@
 package controllers
 
-import play.api.libs.json.Json
-import play.api.libs.ws.WS
+import model.Users
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.ws.{WSResponse, WS}
 import play.api.mvc._
 import play.api.Play.current
 import scala.concurrent.Await
@@ -23,20 +24,44 @@ object Application extends Controller {
     Ok("Fue POST -> \n" + body)
   }}
 
-  def sendGCM = Action{ implicit req => {
-    val msg = Json.obj("data" -> Json.obj(
-      "score" -> "5x1",
-      "time" -> "15:10"),
-    "to" -> "cQlhpkpFEq4:APA91bGt0eocVRSepSP0TukasdVqsXokrRHi3x65-OBNehy_sSdmztbtBj6Hit4Y21094XeLHGCVY36beHjnKgxjrCU835Tz3YYpBEWqbly4RPENfw0m4wR0BxlNgv1Ghu-DjNHBVDSD")
-
+  def sendGCM(msg: JsValue): WSResponse = {
     val send = WS.url("https://gcm-http.googleapis.com/gcm/send")
       .withHeaders(
         ("Content-Type", "application/json"),
         ("Authorization", "key=AIzaSyDF1bcfh7XGvyeD7RKVsMNW5nG523NR__M"))
     .post(msg)
-    val res = Await.result(send, Duration.Inf)
-    Ok(res.body)
+    Await.result(send, Duration.Inf)
+  }
+
+  def sendNotification(user: String) = Action{ implicit req => {
+    val id = Users.getId(user)
+    val msg = Json.obj("to" -> id)
+
+    Ok(sendGCM(msg).body)
+  }}
+
+  def sendMessage(user: String) = Action{ implicit req => {
+    val data = req.body.asJson.getOrElse(Json.obj())
+    val id = Users.getId(user)
+    val msg = Json.obj(
+      "data" -> data,
+      "to" -> id
+    )
+
+    Ok(sendGCM(msg).body)
   }
 
   }
+
+  def registerAndroid = Action{ implicit req => {
+    req.method match {
+      case "GET" => (req.getQueryString("user").get, req.getQueryString("id").get)
+      case "POST" => {
+        val body = req.body.asJson.get
+        (body.\("user"), body.\("id"))
+      }
+    }
+
+    Ok(req.method)
+  }}
 }
---
+++ app/model/Users.scala
@@ -0,0 +1,21 @@
+package model
+
+/**
+ * Created by edwin on 10/24/2015.
+ */
+case class User(user: String, id: String)
+object Users {
+  val users = List.empty[User]
+
+  def addUser(user: User) = {
+    users.+:(user)
+  }
+
+  def getId(user: String): String = {
+    users.find(_.user == user).fold("")(_.id)
+  }
+
+  def getUsers(): List[String] = {
+    users.map(_.user)
+  }
+}
