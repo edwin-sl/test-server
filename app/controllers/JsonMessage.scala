27c17b40c873bfedf013f7f4481490db171aba0e
edwin-salinas1 <edwin.salinas@stochasticstudio.com>
2015-11-20 02:26:20 -0800

    add joda include timestamp

--- app/controllers/JsonMessage.scala
+++ app/controllers/JsonMessage.scala
@@ -2,6 +2,7 @@ package controllers
 
 import controllers.GCMMessage.MessageType.MessageType
 import org.h2.value.Value
+import org.joda.time.Instant
 import play.api.libs.json.{JsObject, JsValue, Json}
 
 /**
@@ -29,7 +30,8 @@ object GCMMessage {
   def createMessage(t: MessageType, to: String, data: JsValue = Json.obj()): JsValue = {
     Json.obj(
       "data" -> Json.obj("type" -> t.toString)
-       .++(data.as[JsObject]),
+       .++(data.as[JsObject])
+       .++(Json.obj("timestamp" -> (Instant.now().getMillis / 1000).toInt)),
       "to" -> to
     )
   }
@@ -37,7 +39,8 @@ object GCMMessage {
   def createBroadcast(t: MessageType, to: List[String], data: JsValue = Json.obj()): JsValue = {
     Json.obj(
       "data" -> Json.obj("type" -> t.toString)
-        .++(data.as[JsObject]),
+        .++(data.as[JsObject])
+        .++(Json.obj("timestamp" -> (Instant.now().getMillis / 1000).toInt)),
       "registration_ids" -> to
     )
   }
--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ lazy val `root` = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq( jdbc , cache , ws ,
-  "net.debasishg" %% "redisclient" % "3.0"
+  "net.debasishg" %% "redisclient" % "3.0",
+  "joda-time" % "joda-time" % "2.9.1"
 )
